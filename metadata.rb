8803c7275281f3843938705d6da00fa5d08a5de3
Kyle Evans <evans.584@osu.edu>
2018-05-16 20:03:59 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'kbe_rsyslog'
+maintainer 'Kyle Evans'
+maintainer_email 'kylebe@gmail.com'
+license 'all_rights'
+description 'Installs/Configures kbe_rsyslog'
+long_description 'Installs/Configures kbe_rsyslog'
+version '0.1.0'
